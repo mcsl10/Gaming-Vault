80ac4921f1134f3d513a1d9ce2346377b76ebcf0
“Mark <marksonielloka@hotmail.com>
2024-09-17 20:32:04 -0400

    New Routes

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -11,6 +11,9 @@ import GlobalStyles from "../GlobalStyles";
 //Components
 import LandingPage from "./LandingPage";
 import Home from "./Home";
+import ExploreGames from "./ExploreGames";
+import SearchGames from "./SearchGames";
+import Interested from "./Interested"
 import InProgressGames from "./InProgressGames";
 import CompletedGames from "./CompletedGames";
 import NavBar from "./Navbar";
@@ -30,9 +33,13 @@ const App = () => {
         {/* Sign Up Page */}
         <Route path="/signup" element={<SignUp />} />
         {/* Log In Page */}
-        <Route path="login" element={<LogIn />} />
+        <Route path="/login" element={<LogIn />} />
+        {/* Route to search games */}
+        <Route path="/search" element={<SearchGames />} />
+        {/* Route to display user potential interest in games */}
+        <Route path="/interested" element={<Interested />} />
         {/* Route to display all games (Top 500 or Latest 500) */}
-        <Route path="/explore" element={<h1>Explore Games</h1>} />
+        <Route path="/explore" element={<ExploreGames />} />
         {/* Route to display User's in-progress games */}
         <Route path="/in-progress" element={<h1>In Progress</h1>} />
         {/* Route to display User's completed games */}
