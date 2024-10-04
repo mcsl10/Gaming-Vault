cb648716c16cff9ffba3b8978f47dc5bfcad71af
mcsl10 <marksonielloka@hotmail.com>
2024-10-04 02:46:37 -0400

    StrictMode enabled in index

--- client/src/index.js
+++ client/src/index.js
@@ -1,9 +1,12 @@
 import ReactDOM from "react-dom/client"
+import { StrictMode } from "react"
 
 import App from "./components/App"
 
 const root = ReactDOM.createRoot(document.getElementById("root"))
 
 root.render(
+    <StrictMode>
     <App />
+    </StrictMode>
 )
